05b6859d8d35a35c9870191ff47caf5ed42ba313
Anderson da Silva Araujo <andersonturns@gmail.com>
2025-11-06 17:10:02 -0300

    Inclusão das funções de relatorio por matéria e atualização do da função main

--- leitura_de_dados_c.c
+++ leitura_de_dados_c.c
@@ -19,6 +19,47 @@ typedef struct {
 Aluno alunos[MAX_ALUNOS];
 int totalAlunos = 0;
 
+// vetor para mapear a escolha numérica para o nome da matéria no CSV
+char* materias_mapa[] = {
+    "ENGENHARIA DE SOFT AGIL", // 0
+    "PYTHON",                 // 1
+    "C",                      // 2
+    "ANALISE DE PROJETOS" // 3
+};
+#define NUM_OPCOES_MATERIA 4
+
+
+int selecao_materias(){
+     int selecao_materia;
+    
+    while (1) { 
+        printf("\nSelecione a materia a ser mostrada: \n");
+        printf("\n0 - %s\n", materias_mapa[0]);
+        printf("1 - %s\n", materias_mapa[1]);
+        printf("2 - %s\n", materias_mapa[2]);
+        printf("3 - %s\n", materias_mapa[3]);
+        printf("99 - Voltar ao menu principal");
+        printf("\n>>>> ");
+
+        if (scanf("%d", &selecao_materia) != 1) {
+            while (getchar() != '\n' && !feof(stdin));
+            selecao_materia = -1;
+        }
+
+        if (selecao_materia >= 0 && selecao_materia < NUM_OPCOES_MATERIA) {
+            return selecao_materia;
+        }
+        else if (selecao_materia == 99) {
+            return 99;
+        }
+        else {
+            printf("\nOpcao invalida. Por favor, tente novamente.\n");
+        }
+    }
+}
+
+
+
 //função para converter os caracteres em maiusculo
 void converter_para_upper(char* s){
     int i = 0;
@@ -28,6 +69,33 @@ void converter_para_upper(char* s){
         i++;
     }
 }
+
+
+void listarPorMateria(char* materia_buscada) {
+    printf("\n=== LISTA DE ALUNOS NA MATERIA: %s ===\n", materia_buscada);
+    
+    int encontrado = 0;
+    
+    for (int i = 0; i < totalAlunos; i++) {
+        // Compara o nome da matéria do aluno com a matéria buscada
+        if (strcmp(alunos[i].materia, materia_buscada) == 0) {
+            
+            // Imprime os detalhes do aluno/materia
+            printf("%s | %s | %s | %s | %.1f | %.1f | Media: %.2f\n",
+                   alunos[i].materia, alunos[i].nome, alunos[i].ra,
+                   alunos[i].turma, alunos[i].nota1, alunos[i].nota2, alunos[i].media);
+            
+            encontrado = 1;
+        }
+    }
+    
+    if (!encontrado) {
+        printf("Nenhum registro encontrado para a materia '%s'.\n", materia_buscada);
+    }
+}
+
+
+
 
 //função para carregar os dados do arquivo csv
 void carregarCSV() {
@@ -117,6 +185,7 @@ void relatorioMediasPorMateria() {
     }
 }
 
+
 int main() {
     int opcao;
 
@@ -124,9 +193,10 @@ int main() {
 
     do {
         printf("\n========= MENU =========\n");
-        printf("1 - Listar todos os alunos e materias\n");
+        printf("1 - Listar todos os dados de alunos e materias\n");
         printf("2 - Buscar aluno por RA\n");
         printf("3 - Relatorio de medias por disciplina\n");
+        printf("4 - Selecionar materia para relatorio\n");
         printf("0 - Sair\n");
         printf("========================\n");
         printf("Escolha uma opcao: ");
@@ -142,16 +212,29 @@ int main() {
         switch (opcao) {
             case 1:
                 listarAlunos();
+
                 break;
             case 2:
                 buscarPorRA();
+
                 break;
             case 3:
                 relatorioMediasPorMateria();
                 break;
+
+            case 4:
+            {int selecao = selecao_materias();            
+            if (selecao >= 0 && selecao < NUM_OPCOES_MATERIA) 
+            {listarPorMateria(materias_mapa[selecao]);} 
+            else if (selecao == 99) 
+            {printf("Retornando ao menu principal.\n");}
+            }
+            break;
+
             case 0:
                 printf("Programa encerrado.\n");
                 break;
+
             default:
                 printf("Escolha invalida.\n");
         }
